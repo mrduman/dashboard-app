5f47b172a7825567cc3afb3afd16018e57bbab61
mrduman <omer.duman@gmx.de>
2024-02-28 19:34:49 +0100

    menu created and styling adjusted

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
 import "./menu.scss";
+import { menu } from "../../data";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => {
+        return (
+          <div className="item">
+            <span className="title">{item.title}</span>
+            {item.listItems.map((listItem) => {
+              return (
+                <Link key={listItem.id} to={listItem.url} className="listItem">
+                  <img src={listItem.icon} alt="" />
+                  <span className="listItemTitle">{listItem.title}</span>
+                </Link>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
