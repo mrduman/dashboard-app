8b83c5e92f9cba7976faaf431586c836a67d060d
mrduman <omer.duman@gmx.de>
2024-02-28 16:26:48 +0100

    initial pages created

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-function App() {
+import Home from "./pages/home/Home";
 
+function App() {
   return (
-    <div>Hello World</div>
-  )
+    <div>
+      <Home />
+    </div>
+  );
 }
 
-export default App
+export default App;
