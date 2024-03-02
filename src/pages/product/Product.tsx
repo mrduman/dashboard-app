c4253e468c5cb7e462428dd6e29eb9974e2fbfb2
mrduman <omer.duman@gmx.de>
2024-03-02 15:35:00 +0100

    single product and user page created

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,7 +1,15 @@
 import "./product.scss";
+import Single from "../../components/single/Single";
+import { singleProduct } from "../../data";
 
 const Product = () => {
-  return <div className="user">Product</div>;
+  return (
+    <div className="product">
+      <div className="productContainer">
+        <Single {...singleProduct} />
+      </div>
+    </div>
+  );
 };
 
 export default Product;
